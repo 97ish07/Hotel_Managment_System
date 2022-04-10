e772de22e2b7f9aa1d00b886c9a5e74f2942e9bb
97ish07 <ishani9799@gmail.com>
2022-04-10 15:46:43 +0530

    create dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+         <link rel="stylesheet" href="/css/main.css"/>
+    </head>
+
+    <body >
+    
+        <div class="background" >
+            
+            <div class="dashboardback"></div>
+                
+                <div class="navigationbar">
+
+                    <div class="logo"></div>
+                    <div class="home" ><div class="homelogo" ></div>Home</div>
+                    <div class="RoomBooking" ><div class="RoomBookinglogo" ></div>Room Booking</div>
+                    <div class="RoomManagement" ><div class="RoomManagementlogo" ></div>Room Management</div>
+                    <div class="Dining" ><div class="Dininglogo" ></div>Dining</div>
+                    <div class="Inventory" ><div class="Inventorylogo" ></div>Inventory</div>
+
+                </div>
+        </div>
+    
+    </body>
+
+</html>
