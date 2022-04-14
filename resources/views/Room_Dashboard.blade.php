6700c18eff88e607509e9a9284e3e7cce7ee5449
97ish07 <ishani9799@gmail.com>
2022-04-14 08:56:59 +0530

    Create Room Manamgent Dashboard

---
+++ resources/views/Room_Dashboard.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+    
+         <link rel="stylesheet" href="/css/main.css"/>
+         <link rel="stylesheet" href="/css/inventorydashboard.css"/>
+         <link rel="stylesheet" href="/js/calendar.js"/>
+         <link rel="stylesheet" href="/css/calendar.css"/>
+    </head>
+    
+    <body >
+    
+        <div class="background" >
+                 
+            <div class="invdashboardback"></div>
+
+            
+
+                 <a href="add_inventory" class="addinventorybtn"> Add Room</a>
+                 <a href="view_inventory" class="viewinventorybtn" > View Room </a> 
+                 <a href="add_inventory" class="DeleteInventorybtn">Delete Room</a>
+                 <a href="view_inventory" class="Reportbtn" > Genarate Report  </a> 
+                    
+                    <div class="navigationbar">
+                  
+                             <a href="dashboard">  <div class="logo"></div></a>
+                             <a href="dashboard"> <div class="home" ><div class="homelogo" ></div>Home</div></a>
+                             <a href="#"> <div class="RoomBooking" ><div class="RoomBookinglogo" ></div>Room Booking</div></a>
+                             <a href="#"><div class="RoomManagement" ><div class="RoomManagementlogo" ></div>Room Management</div></a>
+                             <a href="#"> <div class="Dining" ><div class="Dininglogo" ></div>Dining</div></a>
+                             <a href="inventory_page"><div class="Inventory" ><div class="Inventorylogo" ></div>Inventory Management</div></a>
+
+                    </div>
+        </div>
+    
+    </body>
+
+</html>
