99e79cd9de08fbd71c492fcaaec0d33c5b4bbb0c
sadishawikum <sadishavikum@gmail.com>
2022-05-05 18:05:58 +0530

    creat route for delete

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::view('test','test');
 // Route::get('test',[InventoryController::class,'ShowInventoryDatas']);
 Route::get('test',[InventoryController::class,'ShowInventoryDataTest']);
 
-//pdf genrateeee
+//pdf genratee
 
 Route::get('/nventorypdf',[EventController::class,'exportEventPDF']);
 
